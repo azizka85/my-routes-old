4d8bae47d76985d108dc55fba117bb50ff45b614
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-12-21 16:53:58 +0600

    fix

--- src/client/views/components/auth-service-component.spec.ts
+++ src/client/views/components/auth-service-component.spec.ts
@@ -2,6 +2,8 @@ import { JSDOM } from "jsdom";
 
 import { DEFAULT_LANGUAGE } from '../../../globals';
 
+import { MDCRipple } from '@material/ripple';
+
 import { locales } from '../../../server/helpers/locale-helpers';
 
 import { SignUpPage } from '../pages/sign-up-page';
@@ -26,7 +28,11 @@ describe('SignInPage test', () => {
 					class="main-card__title main-card--justify mdc-typography--headline6"
 				>
 					Or use the service
-				</h6>						
+				</h6>					
+				<a href="#" class="mdc-button" data-button="auth-service-google" title="Google">
+					<span class="mdc-button__ripple"></span>
+					<svg class="main-card__service-icon" viewBox="0 0 16 16"></svg>    
+				</a>	
 			</div>
 		`;
 
@@ -43,6 +49,11 @@ describe('SignInPage test', () => {
 		expect(component['titleElem']).toBeInstanceOf(HTMLElement);
 		expect(component['titleElem']?.getAttribute('data-title')).toEqual('auth-service');
 		expect(component['titleElem']?.textContent).toContain('Or use the service');
+
+		expect(component['googleBtn']).toBeTruthy();
+		expect(component['googleBtn']).toBeInstanceOf(MDCRipple);
+		expect(component['googleBtn']?.root?.getAttribute('data-button')).toEqual('auth-service-google');
+		expect((component['googleBtn']?.root as HTMLElement)?.title).toEqual('Google');
 
 		await SignUpPage.instance.load('ru', {
 			fragment: '',
--- src/client/views/components/auth-service-component.ts
+++ src/client/views/components/auth-service-component.ts
@@ -1,12 +1,22 @@
 import * as router from '@azizka/router';
 
+import { MDCRipple } from '@material/ripple';
+
 import { Component, Page } from "../view";
 
 export class AuthServiceComponent implements Component {
   protected titleElem: HTMLElement | null = null;
 
+  protected googleBtn: MDCRipple | null = null;
+
   async init(page: Page, firstTime: boolean) {
     this.titleElem = page.elem?.querySelector('[data-title="auth-service"]') || null;
+
+    const googleBtnElem = page.elem?.querySelector('[data-button="auth-service-google"]');
+
+    if(googleBtnElem) {
+      this.googleBtn = new MDCRipple(googleBtnElem);
+    }
   }
 
   async load(lang: string, page: router.Page, firstLoad: boolean) {
